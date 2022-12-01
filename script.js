883c1bd63d890d66e2b4aa833fd87bdd2f83581c
SHAJAN29 <118879955+SHAJAN29@users.noreply.github.com>
2022-12-01 09:40:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,43 @@
 // console.log(arr.push(mov.title),arr.push(mov.rating))
 
 
+// class Circle{
+
+// constructor(radius=1.0, color ="green") {
+    
+// this.radius =radius;
+// this.color =color;
+
+// }
+// get radiusCircle(){
+
+// return this.radius
+
+// }
+// get colorCircle(){
+
+//     return this.color
+
+// }
+
+// set radiusCircle(radius){
+
+// this.radius =radius;
+
+// }
+// set colorCircle(color){
+
+//     this.color =color;
+    
+//     }
+
+// }
+
+// let object =new Circle(2.0,"violet");
+
+// object.radiusCircle=5
+
+// object.colorCircle="black"
+
+// console.log(object.radiusCircle)
+// console.log(object.colorCircle)
