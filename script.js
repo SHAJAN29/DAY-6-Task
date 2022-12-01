18c103ceb1abc858f89f9a756298eacf3b273cd8
SHAJAN29 <118879955+SHAJAN29@users.noreply.github.com>
2022-12-01 08:43:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// class Movie{
+
+// constructor(title,studio,rating)
+// {
+
+// this.title=title
+// this.studio=studio
+// this.rating=rating
+
+// }
+// get PG(){
+
+// return `${this.title}`;
+
+// }
+
+// }
+// let flim =new Movie("Casino Royale","Eon Productions","PG­13")
+
+// let arr =[];
+// let mov =flim.PG;
+
+
+// console.log(arr.push(mov.title),arr.push(mov.rating))
+
+
